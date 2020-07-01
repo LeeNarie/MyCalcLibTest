43363d789782d6ab346454b5f24b0d144f8c6764
LeeNarie <61288801+LeeNarie@users.noreply.github.com>
2020-07-02 02:33:31 +0500

    Add files via upload

--- MyCalcTest/UnitTest1.cs
+++ MyCalcTest/UnitTest1.cs
@@ -19,5 +19,30 @@ namespace MyCalcTest
 
 
         }
+
+        public void Min_50_30_is20()
+        {
+
+            int x = 50;
+            int y = 30;
+            int exp = 20;
+            MyCalcLib.MyCalc c = new MyCalcLib.MyCalc();
+            int act = c.Minus(x, y);
+            Assert.AreEqual(exp, act);
+
+
+        }
+        public void Umn_2_3_is6()
+        {
+
+            int x = 2;
+            int y = 3;
+            int exp = 6;
+            MyCalcLib.MyCalc c = new MyCalcLib.MyCalc();
+            int act = c.Umn(x, y);
+            Assert.AreEqual(exp, act);
+
+
+        }
     }
 }
