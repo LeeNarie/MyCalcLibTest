86ffcf91f5f4733eb807c72bf4731cdef693161f
leenarie <KATIDARK@MAIL.RU>
2020-07-02 02:20:13 +0500

    Добавьте файлы проекта.

---
+++ MyCalcTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalcTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("584f4571-1cf0-45a5-b072-fdd611cc0525")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyCalcTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Sum_10_20_is30()
+        {
+
+            int x = 10;
+            int y = 20;
+            int exp = 30;
+            MyCalcLib.MyCalc c = new MyCalcLib.MyCalc();
+            int act = c.Sum(x, y);
+            Assert.AreEqual(exp, act);
+
+
+        }
+    }
+}
